a2a504e424ac0cfbcfc0530c3b2c5cf9d5912358
vladoskav <vladoskavkniaz@yandex.ru>
2020-05-07 22:37:27 +0300

    Добавить компонент инстаграм и его элементы

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+    <instagram></instagram>
   </div>
 </template>
 
 <script>
+import LinkInstagram from '@/components/LinkInstagram';
 export default {
+  components: {
+    instagram: LinkInstagram,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
